ddec6b150cfec67506a9772f67f7078cce1a9e11
ZainChen <2384439266@qq.com>
2023-04-06 17:17:05 +0800

    update docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const lightCodeTheme = require('prism-react-renderer/themes/github')
-const darkCodeTheme = require('prism-react-renderer/themes/oceanicNext')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
 const path = require('path')
 
 /** @type {import('@docusaurus/types').Config} */
@@ -87,7 +87,7 @@ const config = {
           {
             type: 'docSidebar',
             position: 'left',
-            sidebarId: 'note',
+            sidebarId: 'sidebarIdNote',
             label: 'Note',
           },
           {
